a51cd86c74564e8a4689d8363cf040d3e55ed365
APCS3rd <APCS3rd@MHS-Dreyer-03>
2018-09-11 10:08:27 -0700

    synced Accesss Calculate and Library to Github

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+/*This class is client code that uses the Calculate Library to perform various mathematical tasks
+ * 
+ * @author Jakin Chan
+ * @version September 6, 2018
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,12 @@
+/*This class contains methods that perform various math operations
+ * 
+ * @author Jakin Chan
+ * @version September 6, 2018
+ */
+
+public class Calculate {
+	//return the square of the input
+	public static int square(int num) {
+		return num * num;
+	}
+}
