dfe3bcb3f3cb450b05a60c689cb64e9d0bd6db0a
APCS3rd <APCS3rd@MHS-Dreyer-03>
2018-09-11 11:07:37 -0700

    created methods for math operations up to foil

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -6,7 +6,16 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		
+		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average(5, 10));
+		System.out.println(Calculate.average(5, 10, 3));
+		System.out.println(Calculate.toDegrees(3.14));
+		System.out.println(Calculate.toRadians(180));
+		System.out.println(Calculate.discriminant(2, 3, 4));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -9,4 +9,40 @@ public class Calculate {
 	public static int square(int num) {
 		return num * num;
 	}
+	
+	public static int cube(int num) {
+		return num * num * num;
+	}
+	
+	public static double average(double num1, double num2) {
+		return ((num1 + num2)/2);
+	}
+	
+	public static double average(double num1, double num2, double num3) {
+		return ((num1+num2+num3)/2);
+	}
+	
+	public static double toDegrees(double radian) {
+		return (radian * (180/3.14159));
+	}
+	
+	public static double toRadians(double degrees) {
+		return (degrees * (3.14159/180));
+	}
+	
+	public static double discriminant(double a, double b, double c) {
+		return (b * b) - 4 * a *c;
+	}
+	
+	public static String toImproperFrac(int wholenum, int numerator, int denominator) {
+		return ((wholenum*denominator)+numerator) + "/" + denominator;
+	}
+	
+	public static String toMixedNum(int numerator, int denominator) {
+		return ((numerator / denominator) + "_" + (numerator % denominator) + "/" + denominator);
+	}
+	
+	public static String foil(int a, int b, int c, int d, String variable) {
+		return ((a * c) + variable + "^2 + " + ((a * d) + (b + c)));
+	}
 }
