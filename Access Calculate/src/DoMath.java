d96605e8a5594d50ca84b6ac05d208689389fd9c
ggigy <ggigy@LAPTOP-5N1V4DJ4>
2018-09-17 21:01:48 -0700

    Added comments and cleaned up method gcf

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -25,7 +25,7 @@ public class DoMath {
 		System.out.println(Calculate.exponent(2, 3));
 		System.out.println(Calculate.factorial(4));
 		System.out.println(Calculate.isPrime(20));
-		System.out.println(Calculate.gcf(230, 350));
+		System.out.println(Calculate.gcf(250, 350));
 		System.out.println(Calculate.sqrt(2));
 	}
 
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -67,13 +67,13 @@ public class Calculate {
 	
 	public static double max(double num1, double num2, double num3) {
 		double MaxValue;
-		if(num1 > num2) {
+		if(num1 > num2) {//takes the larger value between num1 and num2
 			MaxValue = num1;
 		} else {
 			MaxValue = num2;
 		}
 		
-		if(MaxValue > num3) {
+		if(MaxValue > num3) {//takes the final larger number
 			return MaxValue;
 		} else {
 			return num3;
@@ -106,6 +106,8 @@ public class Calculate {
 	
 	public static int factorial(int num){
 		int factorialResult = 1;
+		
+		//loops through every number between num and 0 and multiplies it
 		for(int i = num; i> 0; i--){
 			factorialResult *= i;
 		}
@@ -114,6 +116,8 @@ public class Calculate {
 
 	public static boolean isPrime(int num){
 		boolean isItPrime = false;
+		
+		//goes through all numbers between 0 and num to see if it is divisible
 		for (int i = 2; i < num; i++){
 			if(isDivisibleBy(num, i) == true){
 				isItPrime = true;
@@ -124,15 +128,9 @@ public class Calculate {
 
 	public static int gcf(int num1, int num2){
 		int commonFactor = 1;
-		int largerNum;
 		
-		if(num1 > num2){ //finds the larger or the two numbers to be the parameter for the for loop
-			largerNum = num1;
-		}else{
-			largerNum = num2;
-		}
-		
-		for(int i = 1; i < largerNum; i++){ //calculates and returns the gcf
+		//calculates and returns the gcf
+		for(int i = 1; i <= num1; i++){
 			if(isDivisibleBy(num1, i) == true && isDivisibleBy(num2, i) == true){
 				if(i > commonFactor){
 					commonFactor = i;
@@ -144,6 +142,8 @@ public class Calculate {
 
 	public static double sqrt(double num){
 		double guess = num;
+		
+		//keeps guessing until the difference between guess^2 and num is less than 0.005
 		while(absValue(num - (guess * guess)) >= 0.005){
 			guess = (0.5 * ((num/guess) + guess));
 		}
